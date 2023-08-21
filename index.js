4168fcef513112998169edf6460fc1f404059bf5
Daniel McGrath <djmcgrath95@gmail.com>
2023-08-21 17:44:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const array = [];
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    
+    return array;
+
+}
+
+function countDown(number) {
+    let n = number; 
+    while (n >= 0) {
+        console.log(n--);
+    }
+
+    return n;
+}
